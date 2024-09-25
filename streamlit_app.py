b671cd252734b823dc1975de6bd9f14e9e8e27f6
KeshavKokande <56793321+KeshavKokande@users.noreply.github.com>
2024-09-25 11:14:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,7 @@ if ingredients_list:
         # st.write(my_insert_stmt)
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered with the name ' + name_on_order, icon="✅")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
